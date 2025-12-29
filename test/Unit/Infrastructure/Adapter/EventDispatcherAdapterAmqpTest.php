362351c874e6e86dfa6fb09d3779d84e6a778815
Luiz Carvalho <luizottavio49@gmail.com>
2025-12-29 10:26:03 -0300

    test: event dispatcher adapter amqp

--- test/Unit/Infrastructure/Adapter/EventDispatcherAdapterAmqpTest.php
+++ test/Unit/Infrastructure/Adapter/EventDispatcherAdapterAmqpTest.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace HyperfTest\Unit\Infrastructure\Adapter;
 
 use App\Application\Contract\EventDispatcherInterface;
+use App\Domain\Event\TransferCompletedEvent;
 use App\Infrastructure\Adapter\EventDispatcherAdapterAmqp;
+use App\Infrastructure\Amqp\Producer\TransferCompletedProducer;
 use Hyperf\Amqp\Producer;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use function Hyperf\Config\config;
 
 final class EventDispatcherAdapterAmqpTest extends TestCase
 {
@@ -21,180 +24,102 @@ final class EventDispatcherAdapterAmqpTest extends TestCase
         $this->eventDispatcher = new EventDispatcherAdapterAmqp($this->producer);
     }
 
-//    public function testDispatchWithMappedEvent(): void
-//    {
-//        $event = new TransferCompletedEvent('transaction-uuid');
-//        $mappedEventClass = 'App\\Infrastructure\\Amqp\\Producer\\TransferCompletedProducer';
-//
-//        config(['events' => [
-//            TransferCompletedEvent::class => $mappedEventClass
-//        ]]);
-//
-//        $this->producer
-//            ->expects($this->once())
-//            ->method('produce')
-//            ->with($this->callback(function ($producer) use ($event) {
-//                return $producer instanceof TransferCompletedProducer && $producer->event === $event;
-//            }));
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
-//
-//    public function testDispatchWithUnmappedEvent(): void
-//    {
-//        $event = new TransferCompletedEvent('transaction-uuid');
-//
-//        config(['events' => []]);
-//
-//        $this->producer
-//            ->expects($this->never())
-//            ->method('produce');
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
-//
-//    public function testDispatchWithNullMappedEvent(): void
-//    {
-//        $event = new TransferCompletedEvent('transaction-uuid');
-//
-//        config(['events' => [
-//            TransferCompletedEvent::class => null
-//        ]]);
-//
-//        $this->producer
-//            ->expects($this->never())
-//            ->method('produce');
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
-//
-//    public function testDispatchWithEmptyEventsConfig(): void
-//    {
-//        $event = new TransferCompletedEvent('transaction-uuid');
-//
-//        config(['events' => []]);
-//
-//        $this->producer
-//            ->expects($this->never())
-//            ->method('produce');
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
-//
-//    public function testDispatchWithMultipleEventsConfig(): void
-//    {
-//        $event = new TransferCompletedEvent('transaction-uuid');
-//        $mappedEventClass = 'App\\Infrastructure\\Amqp\\Producer\\TransferCompletedProducer';
-//
-//        config(['events' => [
-//            'SomeOtherEvent' => 'SomeOtherProducer',
-//            TransferCompletedEvent::class => $mappedEventClass,
-//            'AnotherEvent' => 'AnotherProducer'
-//        ]]);
-//
-//        $this->producer
-//            ->expects($this->once())
-//            ->method('produce')
-//            ->with($this->callback(function ($producer) use ($event) {
-//                return $producer instanceof $mappedEventClass && $producer->event === $event;
-//            }));
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
-//
-//    public function testDispatchWithInvalidMappedEventClass(): void
-//    {
-//        $event = new TransferCompletedEvent('transaction-uuid');
-//        $invalidEventClass = 'NonExistentClass';
-//
-//        config(['events' => [
-//            TransferCompletedEvent::class => $invalidEventClass
-//        ]]);
-//
-//        $this->producer
-//            ->expects($this->once())
-//            ->method('produce')
-//            ->willThrowException(new \Error("Class '$invalidEventClass' not found"));
-//
-//        $this->expectException(\Error::class);
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
-//
-//    public function testDispatchWithProducerException(): void
-//    {
-//        $event = new TransferCompletedEvent('transaction-uuid');
-//        $mappedEventClass = 'App\\Infrastructure\\Amqp\\Producer\\TransferCompletedProducer';
-//
-//        config(['events' => [
-//            TransferCompletedEvent::class => $mappedEventClass
-//        ]]);
-//
-//        $this->producer
-//            ->expects($this->once())
-//            ->method('produce')
-//            ->willThrowException(new \RuntimeException('Producer error'));
-//
-//        $this->expectException(\RuntimeException::class);
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
-//
-//    public function testDispatchWithDifferentEventTypes(): void
-//    {
-//        $transferEvent = new TransferCompletedEvent('transaction-uuid');
-//        $mappedEventClass = 'App\\Infrastructure\\Amqp\\Producer\\TransferCompletedProducer';
-//
-//        config(['events' => [
-//            TransferCompletedEvent::class => $mappedEventClass
-//        ]]);
-//
-//        $this->producer
-//            ->expects($this->once())
-//            ->method('produce')
-//            ->with($this->callback(function ($producer) use ($transferEvent) {
-//                return $producer instanceof $mappedEventClass && $producer->event === $transferEvent;
-//            }));
-//
-//        $this->eventDispatcher->dispatch($transferEvent);
-//    }
-//
-//    public function testDispatchWithEventContainingSpecialCharacters(): void
-//    {
-//        $transactionId = 'transaction-uuid_with-special-chars!@#$%^&*()';
-//        $event = new TransferCompletedEvent($transactionId);
-//        $mappedEventClass = 'App\\Infrastructure\\Amqp\\Producer\\TransferCompletedProducer';
-//
-//        config(['events' => [
-//            TransferCompletedEvent::class => $mappedEventClass
-//        ]]);
-//
-//        $this->producer
-//            ->expects($this->once())
-//            ->method('produce')
-//            ->with($this->callback(function ($producer) use ($event) {
-//                return $producer instanceof $mappedEventClass && $producer->event === $event;
-//            }));
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
-//
-//    public function testDispatchWithEmptyTransactionId(): void
-//    {
-//        $event = new TransferCompletedEvent('');
-//        $mappedEventClass = 'App\\Infrastructure\\Amqp\\Producer\\TransferCompletedProducer';
-//
-//        config(['events' => [
-//            TransferCompletedEvent::class => $mappedEventClass
-//        ]]);
-//
-//        $this->producer
-//            ->expects($this->once())
-//            ->method('produce')
-//            ->with($this->callback(function ($producer) use ($event) {
-//                return $producer instanceof $mappedEventClass && $producer->event === $event;
-//            }));
-//
-//        $this->eventDispatcher->dispatch($event);
-//    }
+    public function testDispatchWithMappedEvent(): void
+    {
+        $event = new TransferCompletedEvent('transaction-uuid');
+
+        $this->producer
+            ->expects($this->once())
+            ->method('produce')
+            ->with($this->callback(function ($producer) use ($event) {
+                return $producer instanceof TransferCompletedProducer;
+            }));
+
+        $this->eventDispatcher->dispatch($event);
+    }
+
+    public function testDispatchWithMultipleEventsConfig(): void
+    {
+        $event = new TransferCompletedEvent('transaction-uuid');
+
+        $this->producer
+            ->expects($this->once())
+            ->method('produce')
+            ->with($this->callback(function ($producer) use ($event) {
+                return $producer instanceof TransferCompletedProducer;
+            }));
+
+        $this->eventDispatcher->dispatch($event);
+    }
+
+    public function testDispatchWithInvalidMappedEventClass(): void
+    {
+        $event = new TransferCompletedEvent('transaction-uuid');
+
+        $this->producer
+            ->expects($this->once())
+            ->method('produce')
+            ->willThrowException(new \Error("Class 'NonExistentClass' not found"));
+
+        $this->expectException(\Error::class);
+
+        $this->eventDispatcher->dispatch($event);
+    }
+
+    public function testDispatchWithProducerException(): void
+    {
+        $event = new TransferCompletedEvent('transaction-uuid');
+
+        $this->producer
+            ->expects($this->once())
+            ->method('produce')
+            ->willThrowException(new \RuntimeException('Producer error'));
+
+        $this->expectException(\RuntimeException::class);
+
+        $this->eventDispatcher->dispatch($event);
+    }
+
+    public function testDispatchWithDifferentEventTypes(): void
+    {
+        $transferEvent = new TransferCompletedEvent('transaction-uuid');
+
+        $this->producer
+            ->expects($this->once())
+            ->method('produce')
+            ->with($this->callback(function ($producer) use ($transferEvent) {
+                return $producer instanceof TransferCompletedProducer;
+            }));
+
+        $this->eventDispatcher->dispatch($transferEvent);
+    }
+
+    public function testDispatchWithEventContainingSpecialCharacters(): void
+    {
+        $transactionId = 'transaction-uuid_with-special-chars!@#$%^&*()';
+        $event = new TransferCompletedEvent($transactionId);
+
+        $this->producer
+            ->expects($this->once())
+            ->method('produce')
+            ->with($this->callback(function ($producer) use ($event) {
+                return $producer instanceof TransferCompletedProducer;
+            }));
+
+        $this->eventDispatcher->dispatch($event);
+    }
+
+    public function testDispatchWithEmptyTransactionId(): void
+    {
+        $event = new TransferCompletedEvent('');
+
+        $this->producer
+            ->expects($this->once())
+            ->method('produce')
+            ->with($this->callback(function ($producer) use ($event) {
+                return $producer instanceof TransferCompletedProducer;
+            }));
+
+        $this->eventDispatcher->dispatch($event);
+    }
 }
